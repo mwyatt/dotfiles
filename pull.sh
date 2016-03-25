314d5a08be31788de6a67b45b1eb3db3ccbd9372
Martin <martin.wyatt@gmail.com>
2016-03-25 17:28:37 +0000

    push pull -R and added new xfe settings

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ~/.config/xfce4/* xfce4/
-cp ~/.config/sublime-text-3/Packages/User/* sublime-text/
+cp -R ~/.config/xfce4/* xfce4/
+cp -R ~/.config/sublime-text-3/Packages/User/* sublime-text/
 
 echo 'pulled settings into this repo'
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp xfce4/* ~/.config/xfce4/
-cp sublime-text/* ~/.config/sublime-text-3/Packages/User/
+cp -R xfce4/* ~/.config/xfce4/
+cp -R sublime-text/* ~/.config/sublime-text-3/Packages/User/
 
 echo 'pushed settings to ~/.config/'
